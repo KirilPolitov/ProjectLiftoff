9e684019780bd07287fb90f2391744697178b109
KirilPolitov <polito.kiril@gmail.com>
2026-01-27 16:03:55 +0200

    v3.1.0 source and font

--- ProjectLiftoff.cpp
+++ ProjectLiftoff.cpp
@@ -6,11 +6,16 @@
 #include <atomic>
 #include <iostream>
 #include <cmath>
+#include <string>
+#include <ctime>
+#include <cstdlib>
 #pragma comment(linker, "/SUBSYSTEM:windows /ENTRY:mainCRTStartup")
 
 static std::atomic<bool> stopSound{ false };
 static std::atomic<bool> thrusterOn{ false };
 static std::atomic<bool> dead{ false };
+static std::atomic<float> musicVolume = 50.f;
+static std::atomic<float> engineMaxVol = 100.f;
 
 static void ambientSoundThread() {
     sf::Music ambientSound;
@@ -18,12 +23,13 @@ static void ambientSoundThread() {
         return;
     }
     ambientSound.setLooping(true);
-    ambientSound.setVolume(50.f);
+    ambientSound.setVolume(musicVolume);
     ambientSound.play();
 
     // Keep the thread alive
     while (!stopSound.load() && ambientSound.getStatus() == sf::SoundSource::Status::Playing) {
         std::this_thread::sleep_for(std::chrono::milliseconds(30));
+        ambientSound.setVolume(musicVolume);
     }
 }
 
@@ -40,13 +46,14 @@ static void thrusterSound() {
         if (!thrusterOn.load()) {
             engine.setVolume(0.f);
         }
-        else engine.setVolume(100.f);
+        else engine.setVolume(engineMaxVol);
         std::this_thread::sleep_for(std::chrono::milliseconds(30));
     }
 }
 
 
 int main() {
+    srand(time(0));
     // Use the desktop resolution so the video mode matches the screen size
     sf::VideoMode videoMode = sf::VideoMode::getDesktopMode();
     sf::RenderWindow window(videoMode, "Project Lifoff");
@@ -54,6 +61,56 @@ int main() {
     window.setVerticalSyncEnabled(true);
 	sf::Image icon;
     if (icon.loadFromFile("Cartoon_space_rocket.png")) { window.setIcon(icon); }
+	sf::RenderTexture userInterface({ videoMode.size.x, videoMode.size.y });
+	userInterface.setView(window.getDefaultView());
+    float uiScale = window.getSize().y / 1080.f;
+    float uiScaleX = window.getSize().x / 1920.f;
+    sf::Font font("spaceAge.ttf");
+    sf::Text velText(font);
+	velText.setCharacterSize(30 * uiScale);
+	velText.setFillColor(sf::Color::White);
+	velText.setPosition({ 10.f, 10.f });
+	sf::Text atmoStressText(font);
+	atmoStressText.setCharacterSize(30 * uiScale);
+	atmoStressText.setFillColor(sf::Color::White);
+	atmoStressText.setPosition({ 10.f, 60.f });
+    sf::Text planetRelVel(font);
+    planetRelVel.setCharacterSize(30 * uiScale);
+    planetRelVel.setFillColor(sf::Color::White);
+    planetRelVel.setPosition({ 10.f, 110.f });
+    sf::Text moonRelVel(font);
+    moonRelVel.setCharacterSize(30 * uiScale);
+    moonRelVel.setFillColor(sf::Color::White);
+    moonRelVel.setPosition({ 10.f, 160.f });
+    sf::Text deathMessage(font);
+    deathMessage.setCharacterSize(200 * uiScale);
+    deathMessage.setFillColor(sf::Color::Red);
+    deathMessage.setString("YOU DIED");
+    deathMessage.setOrigin(deathMessage.getLocalBounds().getCenter());
+    deathMessage.setPosition({ (window.getSize().x / 2.f), (window.getSize().y / 2.f) });
+    std::string collisionDeaths[3] = {"You hit the ground too hard!", "You are bad at falling!", "SPLAT!!!"};
+    std::string aeroDeaths[3] = {"You sure like the wind!", "SLOW DOWN!", "Is it getting hot in here?"};
+    sf::Text deathComment(font);
+    deathComment.setCharacterSize(30 * uiScale);
+    deathComment.setFillColor(sf::Color::White);
+    sf::Text respawnRemind(font);
+    respawnRemind.setCharacterSize(30 * uiScale);
+    respawnRemind.setFillColor(sf::Color::Cyan);
+    respawnRemind.setString("Press Escape to respawn");
+    respawnRemind.setOrigin(respawnRemind.getLocalBounds().getCenter());
+    respawnRemind.setPosition({ (window.getSize().x / 2.f), (window.getSize().y / 1.2f) });
+    sf::Text collisonFatality(font);
+    collisonFatality.setCharacterSize(30 * uiScale);
+    collisonFatality.setFillColor(sf::Color::White);
+    collisonFatality.setString("Fatal collison Speed: 250");
+    collisonFatality.setOrigin(collisonFatality.getLocalBounds().getCenter());
+    collisonFatality.setPosition({ (1580.f * uiScaleX), 30.f });
+    sf::Text aeroFatality(font);
+    aeroFatality.setCharacterSize(30 * uiScale);
+    aeroFatality.setFillColor(sf::Color::White);
+    aeroFatality.setString("Fatal aerodynamic stress: 150");
+    aeroFatality.setOrigin(aeroFatality.getLocalBounds().getCenter());
+    aeroFatality.setPosition({ (1533.f * uiScaleX), 80.f });
     sf::RectangleShape rectangle(sf::Vector2f(100.f, 100.f));
     rectangle.setOrigin(rectangle.getGeometricCenter());
     rectangle.setPosition({ 10.f, 10.f });
@@ -91,6 +148,10 @@ int main() {
     float thrust = 200.f;
     float rotationSpeed = 3000.f;
     sf::View camera(window.getDefaultView());
+    sf::View cameraUI;
+    cameraUI.setSize(window.getView().getSize());
+    cameraUI.setCenter(window.getView().getCenter());
+    userInterface.setView(cameraUI);
 	sf::Vector2f initialMoonDirection = sf::Vector2f(1.f, 0.f);
 	velocityMoon = initialMoonDirection.normalized() * 100.f;
 
@@ -120,10 +181,12 @@ int main() {
 		sf::Vector2f normalizedDirectionToMoon = directionToMoon.normalized();
 		velocity += normalizedDirectionToMoon * (moonGravity / (distanceToMoon * distanceToMoon)) * deltaTime;
 
+        short index = rand() % 3;
         //collision
 		float minDistance = rectangle.getSize().length() / 5.8f + circle.getRadius();
 		if (distance < minDistance && velocity.length() > 250.f) {
             dead.store(true);
+            deathComment.setString(collisionDeaths[index]);
         }
         if (distance < minDistance) {
             sf::Vector2f overlap = normalizedDirection * (minDistance - distance);
@@ -135,6 +198,7 @@ int main() {
 		float moonRelativeVelocity = (velocity - velocityMoon).length();
         if (distanceToMoon < minDistanceToMoon && moonRelativeVelocity > 250.f) {
             dead.store(true);
+            deathComment.setString(collisionDeaths[index]);
         }
         if (distanceToMoon < minDistanceToMoon) {
             sf::Vector2f overlap = normalizedDirectionToMoon * (minDistanceToMoon - distanceToMoon);
@@ -144,11 +208,12 @@ int main() {
 		
 		//atmosphere drag
         float atmosphereDistance = circle.getRadius() * 1.5f;
+        float pressure = 1.f * std::exp(-(distance - minDistance) / ((atmosphereDistance - minDistance) / 3));
+        float stress = pressure * velocity.length() / 2.f;
         if (distance < atmosphereDistance) {
-			float pressure = 1.f * std::exp(-(distance - minDistance) / ((atmosphereDistance - minDistance)/3));
-			float stress = pressure * velocity.length() / 2.f;
             if (stress > 150.f) {
 				dead.store(true);
+                deathComment.setString(aeroDeaths[index]);
 			}
             if (stress > 100.f) {
 				rectangle.setFillColor(sf::Color::Red);
@@ -157,6 +222,14 @@ int main() {
 			velocity -= velocity * pressure * deltaTime;
 		}
 		else rectangle.setFillColor(sf::Color::White);
+
+        //UI elements
+        velText.setString("Velocity: " + std::to_string(static_cast<int>(floor(velocity.length()))));
+        atmoStressText.setString("Atmosphereic Stress: " + std::to_string(static_cast<int>(floor(stress))));
+        planetRelVel.setString("Planet Relative Velocity: " + std::to_string(static_cast<int>(floor(velocity.length()))));
+        moonRelVel.setString("Moon Relative Velocity: " + std::to_string(static_cast<int>(floor(moonRelativeVelocity))));
+        deathComment.setOrigin(deathComment.getLocalBounds().getCenter());
+        deathComment.setPosition({ (window.getSize().x / 2.f), (window.getSize().y / 1.5f) });
 
         while (auto opt = window.pollEvent()) {
             const sf::Event& event = *opt;
@@ -195,6 +268,26 @@ int main() {
 			dead.store(false);
 			rectangle.setPosition({ 10.f, 10.f });
         }
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::I)) {
+            if (musicVolume < 100.f)
+                musicVolume += 100.f * deltaTime;
+            if (musicVolume > 100.f) musicVolume = 100.f;
+        }
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::K)) {
+            if (musicVolume > 0.f)
+                musicVolume -= 100.f * deltaTime;
+            if (musicVolume < 0.f) musicVolume = 0.f;
+        }
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::O)) {
+            if (engineMaxVol < 100.f)
+                engineMaxVol += 100.f * deltaTime;
+            if (engineMaxVol > 100.f) engineMaxVol = 100.f;
+        }
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::L)) {
+            if (engineMaxVol > 0.f)
+                engineMaxVol -= 100.f * deltaTime;
+            if (engineMaxVol < 0.f) engineMaxVol = 0.f;
+        }
 		//update position
 		if (dead.load() == true)
             velocity = sf::Vector2f(0.f, 0.f);
@@ -202,12 +295,34 @@ int main() {
 		moon.move(velocityMoon * deltaTime);
 		//rendering
         window.clear(sf::Color::Black);
+		userInterface.clear(sf::Color::Transparent);
         window.draw(atmosphere);
 		if (!dead.load() == true)
 		    window.draw(rectangle);
         window.draw(moon);
 		window.draw(circle);
+		window.setView(cameraUI);
+		userInterface.draw(velText);
+        if (distance < atmosphereDistance) {
+            userInterface.draw(atmoStressText);
+            userInterface.draw(aeroFatality);
+        }
+        if (distance < 1500.f)
+            userInterface.draw(planetRelVel);
+        if (distanceToMoon < 700.f)
+            userInterface.draw(moonRelVel);
+        if (distance < 1500.f || distanceToMoon < 700.f)
+            userInterface.draw(collisonFatality);
+        if (dead.load() == true) {
+            userInterface.draw(deathMessage);
+            userInterface.draw(deathComment);
+            userInterface.draw(respawnRemind);
+        }
+		sf::Sprite uiSprite(userInterface.getTexture());
+		uiSprite.setPosition(cameraUI.getCenter() - cameraUI.getSize() / 2.f);
+		window.draw(uiSprite);
         window.display();
+        userInterface.display();
     }
     return 0;
 }
